dcafc3a59189dc7086dd206322badaf059c8cea9
cailudys <cailudys@qq.com>
2020-07-23 19:03:25 +0800

    删除无用组件

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
+// 导入vue（单文件组件）包;import后面的名字是自定义的！
 import Vue from 'vue'
+// 导入APP根组件
 import App from './App.vue'
+// 导入路由组件
 import router from './router'
+// 导入element-ui组件
 import './plugins/element.js'
 
 Vue.config.productionTip = false
 
 new Vue({
+  // 把路由挂载到vue实例上
   router,
+  // 通过rander函数把App根组件渲染到页面上
   render: h => h(App)
 }).$mount('#app')
